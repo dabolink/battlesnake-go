1f6ac8c67c711a652600c2e49a61d04396efe206
Daniel Bolink <dabolink@gmail.com>
2018-03-03 13:46:18 -0800

    initial

---
+++ model/move_request.go
@@ -0,0 +1,22 @@
+package model
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+type MoveRequest struct {
+	Food   PointList `json:"food"`
+	Height int       `json:"height"`
+	ID     int       `json:"id"`
+	Snakes SnakeList `json:"snakes"`
+	Turn   int       `json:"turn"`
+	Width  int       `json:"width"`
+	You    Snake     `json:"you"`
+}
+
+func NewMoveRequest(req *http.Request) (*MoveRequest, error) {
+	decoded := MoveRequest{}
+	err := json.NewDecoder(req.Body).Decode(&decoded)
+	return &decoded, err
+}
